182878812584de2dd739df9c10de42fd1d590ab0
coder <dnstylish@gmail.com>
2022-04-12 00:24:11 +0700

    Initial Commit

---
+++ Shared/comixApp.swift
@@ -0,0 +1,17 @@
+//
+//  comixApp.swift
+//  Shared
+//
+//  Created by Yuan on 12/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct comixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
